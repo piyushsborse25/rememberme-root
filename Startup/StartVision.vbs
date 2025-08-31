43a6bf439e3f6043bd401975a1af133a02ef156a
piyushsborse25 <piyushsborse25@gmail.com>
2025-09-01 00:07:37 +0530

    Removes Some files

--- Startup/StartVision.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("Wscript.Shell").Run "vision.bat",0,True
--- Startup/vision.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
-activate vision && ^
-cd /d C:\Users\HP\PycharmProjects\RememberMe && ^
-python Main.py
